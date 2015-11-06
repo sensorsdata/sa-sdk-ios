4ef541989b5ac47b4f4b5cfe7b4d6d61236a73d3
Cao Jiang <caojiang@sensorsdata.cn>
2015-11-06 13:50:05 +0800

    First Commit

---
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsException.h
@@ -0,0 +1,13 @@
+//
+//  SensorsAnalyticsException.h
+//  SensorsAnalyticsSDK
+//
+//  Created by 曹犟 on 15/7/25.
+//  Copyright (c) 2015年 SensorsData. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SensorsAnalyticsException : NSException
+
+@end
---
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SensorsAnalyticsException.m
@@ -0,0 +1,13 @@
+//
+//  SensorsAnalyticsException.m
+//  SensorsAnalyticsSDK
+//
+//  Created by 曹犟 on 15/7/25.
+//  Copyright (c) 2015年 SensorsData. All rights reserved.
+//
+
+#import "SensorsAnalyticsException.h"
+
+@implementation SensorsAnalyticsException
+
+@end
