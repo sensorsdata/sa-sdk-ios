2199c396c2ccf3152886dad15bb16cecf07bb5d5
Cao Jiang <caojiang@sensorsdata.cn>
2015-11-06 13:46:35 +0800

    first commit

---
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/JSONUtil.m
@@ -0,0 +1,102 @@
+//
+//  JSONUtil.m
+//  SensorsAnalyticsSDK
+//
+//  Created by 曹犟 on 15/7/7.
+//  Copyright (c) 2015年 SensorsData. All rights reserved.
+//
+
+#import "JSONUtil.h"
+#import "SALogger.h"
+
+@implementation JSONUtil {
+    NSDateFormatter *_dateFormatter;
+}
+
+- (id)init {
+    self = [super init];
+    _dateFormatter = [[NSDateFormatter alloc] init];
+    [_dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS"];
+    [_dateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC+8"]];
+    return self;
+}
+
+/**
+ *  @abstract
+ *  把一个Object转成Json字符串
+ *
+ *  @param obj 要转化的对象Object
+ *
+ *  @return 转化后得到的字符串
+ */
+- (NSData *)JSONSerializeObject:(id)obj {
+    id coercedObj = [self JSONSerializableObjectForObject:obj];
+    NSError *error = nil;
+    NSData *data = nil;
+    @try {
+        data = [NSJSONSerialization dataWithJSONObject:coercedObj options:0 error:&error];
+    }
+    @catch (NSException *exception) {
+        SAError(@"%@ exception encoding api data: %@", self, exception);
+    }
+    if (error) {
+        SAError(@"%@ error encoding api data: %@", self, error);
+    }
+    return data;
+}
+
+/**
+ *  @abstract
+ *  在Json序列化的过程中，对一些不同的类型做一些相应的转换
+ *
+ *  @param obj 要处理的对象Object
+ *
+ *  @return 处理后的对象Object
+ */
+- (id)JSONSerializableObjectForObject:(id)obj {
+    // valid json types
+    if ([obj isKindOfClass:[NSString class]] ||
+        [obj isKindOfClass:[NSNumber class]] ) {
+        return obj;
+    }
+    // recurse on containers
+    if ([obj isKindOfClass:[NSArray class]]) {
+        NSMutableArray *a = [NSMutableArray array];
+        for (id i in obj) {
+            [a addObject:[self JSONSerializableObjectForObject:i]];
+        }
+        return [NSArray arrayWithArray:a];
+    }
+    if ([obj isKindOfClass:[NSDictionary class]]) {
+        NSMutableDictionary *d = [NSMutableDictionary dictionary];
+        for (id key in obj) {
+            NSString *stringKey;
+            if (![key isKindOfClass:[NSString class]]) {
+                stringKey = [key description];
+                SAError(@"%@ warning: property keys should be strings. got: %@. coercing to: %@", self, [key class], stringKey);
+            } else {
+                stringKey = [NSString stringWithString:key];
+            }
+            id v = [self JSONSerializableObjectForObject:obj[key]];
+            d[stringKey] = v;
+        }
+        return [NSDictionary dictionaryWithDictionary:d];
+    }
+    if ([obj isKindOfClass:[NSSet class]]) {
+        NSMutableArray *a = [NSMutableArray array];
+        for (id i in obj) {
+            [a addObject:[self JSONSerializableObjectForObject:i]];
+        }
+        return [NSArray arrayWithArray:a];
+    }
+    // some common cases
+    if ([obj isKindOfClass:[NSDate class]]) {
+        return [_dateFormatter stringFromDate:obj];
+    }
+    // default to sending the object's description
+    NSString *s = [obj description];
+    SAError(@"%@ warning: property values should be valid json types. got: %@. coercing to: %@", self, [obj class], s);
+    return s;
+}
+
+@end
---
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK/SALogger.h
@@ -0,0 +1,29 @@
+//
+//  SALogger.h
+//  SensorsAnalyticsSDK
+//
+//  Created by 曹犟 on 15/7/6.
+//  Copyright (c) 2015年 SensorsData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifndef __SensorsAnalyticsSDK__SALogger__
+#define __SensorsAnalyticsSDK__SALogger__
+
+static inline void SALog(NSString *format, ...) {
+    __block va_list arg_list;
+    va_start (arg_list, format);
+    NSString *formattedString = [[NSString alloc] initWithFormat:format arguments:arg_list];
+    va_end(arg_list);
+    NSLog(@"[SensorsAnalytics] %@", formattedString);
+}
+
+#define SAError(...) SALog(__VA_ARGS__)
+
+#ifdef DEBUG
+#define SADebug(...) SALog(__VA_ARGS__)
+#else
+#define SADebug(...)
+#endif
+
+#endif /* defined(__SensorsAnalyticsSDK__SALogger__) */
