66444f1b968758a3053bc1b23b8bc9e4b885f952
minchaozhang <minchaozhang@163.com>
2020-04-19 15:14:42 +0800

    Release 2.0.4-pre

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -74,7 +74,7 @@
 #import "SALog+Private.h"
 #import "SAConsoleLogger.h"
 
-#define VERSION @"2.0.3-pre"
+#define VERSION @"2.0.4-pre"
 
 static NSUInteger const SA_PROPERTY_LENGTH_LIMITATION = 8191;
 
@@ -286,7 +286,7 @@ static SensorsAnalyticsSDK *sharedInstance = nil;
             
             dispatch_block_t mainThreadBlock = ^(){
                 //判断被动启动
-                if (UIApplication.sharedApplication.backgroundTimeRemaining != UIApplicationBackgroundFetchIntervalNever) {
+                if (UIApplication.sharedApplication.applicationState == UIApplicationStateBackground) {
                     self->_launchedPassively = YES;
                 }
             };
@@ -2653,10 +2653,8 @@ static void sa_imp_setJSResponderBlockNativeResponder(id obj, SEL cmd, id reactT
 - (void)applicationWillEnterForeground:(NSNotification *)notification {
     SALogDebug(@"%@ application will enter foreground", self);
     
-    if (UIApplication.sharedApplication.applicationState == UIApplicationStateBackground) {
-        _appRelaunched = YES;
-        self.launchedPassively = NO;
-    }
+    _appRelaunched = YES;
+    self.launchedPassively = NO;
 }
 
 - (void)applicationDidBecomeActive:(NSNotification *)notification {
