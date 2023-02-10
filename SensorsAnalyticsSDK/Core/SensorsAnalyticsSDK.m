ae6c3cf7522b555b55f45f9de811f6706d9790bf
王洋洋 <wangyangyang@sensorsdata.cn>
2023-02-10 15:55:02 +0800

    Release 4.5.4

--- SensorsAnalyticsSDK/Core/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/Core/SensorsAnalyticsSDK.m
@@ -64,7 +64,7 @@
 #import "SALimitKeyManager.h"
 #import "NSDictionary+SACopyProperties.h"
 
-#define VERSION @"4.5.3"
+#define VERSION @"4.5.4"
 
 void *SensorsAnalyticsQueueTag = &SensorsAnalyticsQueueTag;
 
